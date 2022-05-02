ae0f8684475cb0379133bb51e9ee2e7919a8e6f9
Ngoan <tvngoan343@gmail.com>
2022-05-02 13:51:42 +0700

    'NewLV'

---
+++ Assets/Script/Exit.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+
+    
+    
+
+    
+    
+     public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Exit game");
+    }
+}
